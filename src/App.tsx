16159826d40372a679c50c68522eafbb50a61d3f
u-leslie <heavenmadeleslie@gmail.com>
2025-11-07 00:29:23 +0200

    refactor: codebase refacor

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { DefaultProviders } from "./components/providers/default.tsx";
-import AuthCallback from "./pages/auth/Callback.tsx";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 export default function App() {
   return (
-    <DefaultProviders>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/auth/callback" element={<AuthCallback />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-    </DefaultProviders>
   );
 }
--- src/components/providers/default.tsx
+++ src/components/providers/default.tsx
@@ -1,29 +1,26 @@
-import { ConvexProviderWithAuth } from "convex/react";
-import { ThemeProvider } from "next-themes";
-import { convex } from "@/lib/convex.ts";
-import { useAuth } from "@/hooks/use-auth.ts";
-import { Toaster } from "@/components/ui/sonner.tsx";
-import { TooltipProvider } from "@/components/ui/tooltip.tsx";
-import { AuthProvider } from "@/components/providers/auth.tsx";
-import { QueryClientProvider } from "@/components/providers/query-client.tsx";
+// import { ConvexProviderWithAuth } from "convex/react";
+// import { ThemeProvider } from "next-themes";
+// import { convex } from "@/lib/convex.ts";
+// import { useAuth } from "@/hooks/use-auth.ts";
+// import { Toaster } from "@/components/ui/sonner.tsx";
+// import { TooltipProvider } from "@/components/ui/tooltip.tsx";
+// import { QueryClientProvider } from "@/components/providers/query-client.tsx";
 
-export function DefaultProviders({ children }: { children: React.ReactNode }) {
-  return (
-    <AuthProvider>
-      <ConvexProviderWithAuth client={convex} useAuth={useAuth}>
-        <QueryClientProvider>
-            <TooltipProvider>
-              <ThemeProvider
-                attribute="class"
-                defaultTheme="dark"
-                enableSystem
-              >
-                <Toaster />
-                {children}
-              </ThemeProvider>
-            </TooltipProvider>
-        </QueryClientProvider>
-      </ConvexProviderWithAuth>
-    </AuthProvider>
-  );
-}
+// export function DefaultProviders({ children }: { children: React.ReactNode }) {
+//   return (
+//       <ConvexProviderWithAuth client={convex} useAuth={useAuth}>
+//         <QueryClientProvider>
+//             <TooltipProvider>
+//               <ThemeProvider
+//                 attribute="class"
+//                 defaultTheme="dark"
+//                 enableSystem
+//               >
+//                 <Toaster />
+//                 {children}
+//               </ThemeProvider>
+//             </TooltipProvider>
+//         </QueryClientProvider>
+//       </ConvexProviderWithAuth>
+//   );
+// }
